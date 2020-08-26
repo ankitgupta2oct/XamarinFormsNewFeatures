be48220800900a816139f19c8191733eb60949f6
Ankit Gupta <sumantrabhattacharyya@Ankit-MBP.domain.name>
2020-08-26 18:34:01 +0530

    Support Animate Gradient

---
+++ Version_4_8/Version_4_8/Version_4_8/ViewModels/GradientsAndBrushesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Version_4_8.Views;
+using Xamarin.Forms;
+
+namespace Version_4_8.ViewModels
+{
+    public class GradientsAndBrushesViewModel : BaseViewModel
+    {
+        public ICommand AnimateGradientCommand { get; }
+
+        public GradientsAndBrushesViewModel()
+        {
+            AnimateGradientCommand = new Command(async () => await OnAnimateGradientCommand().ConfigureAwait(false));
+        }
+
+        private Task OnAnimateGradientCommand()
+        {
+            return Application.Current.MainPage.Navigation.PushAsync(new AnimateGradientPage());
+        }
+    }
+}
---
+++ Version_4_8/Version_4_8/Version_4_8/Views/AnimateGradientPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Version_4_8.Views
+{
+    public partial class AnimateGradientPage : ContentPage
+    {
+        public AnimateGradientPage()
+        {
+            InitializeComponent();
+            Task.Run(AnimateBackground);
+        }
+
+        private async Task AnimateBackground()
+        {
+            Action<double> forward = input => bdGradient.AnchorY = input;
+            Action<double> backward = input => bdGradient.AnchorY = input;
+
+            while (true)
+            {
+                bdGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+                bdGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
